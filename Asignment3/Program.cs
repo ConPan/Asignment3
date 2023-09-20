a7395a7cb13b6ca896cfe5adfeea1f4a203719e2
Constantin Pantesi <constantin.pantesi@yahoo.com>
2023-09-20 17:34:27 +0200

    Add project files.

---
+++ Asignment3/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void MyThirdMethod(string message)
+    {
+        Console.WriteLine(message);
+        string userName = Console.ReadLine();
+        Console.WriteLine(userName);
+    }
+
+    static void Main(string[] args)
+    {
+        MyThirdMethod("Please enter a number");
+    }
+}
+
+
+
+
+
